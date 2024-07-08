1ce5e0c77503c19f0523248999f5a518ab9086e6
DiegoSPrado <159810695+DiegoSPrado@users.noreply.github.com>
2024-07-08 12:33:15 -0300

    atualizando

---
+++ src/Components/Pages/Home/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Login from '../Login/Login';
+
+function App() {
+  return (
+   <div className='App'>
+      <p>Página de Login</p>
+      <Login/>
+   </div>
+  );
+}
+
+export default App
